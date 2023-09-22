7e1042643bda9d983c41eeb9e9182ae353386889
Mabelt83 <137824865+Mabelt83@users.noreply.github.com>
2023-09-22 09:33:18 -0400

    hw

---
+++ MOVIE-THEATER.sql
@@ -0,0 +1,282 @@
+INSERT INTO customer(
+        customer_id,
+        first_name,
+        last_name,
+        phone_number)
+        VALUES(
+        '1',
+        'Mabel',
+        'Torres',
+        '305-123-4567'
+        );
+UPDATE customer
+SET phone_number = '305-123-4567'
+WHERE customer_id = 1;
+        INSERT INTO customer(
+        customer_id,
+        first_name,
+        last_name,
+        phone_number)
+        VALUES(
+        '2',
+        'Rambo'
+        'Torres'
+        '305-123-7654'
+        );
+UPDATE customer
+SET phone_number = '312-555-5555'
+WHERE customer_id = 2;
+INSERT INTO movies(
+        film_id,
+        film_title,
+        film_genre,
+        film_rating,
+        film_duration,
+        film_stereoscopes)
+        VALUES(
+        '1',
+        'The Nun',
+        'Horror',
+        'R',
+        '1hr 39mins',
+        'standard'
+        );
+        INSERT INTO movies(
+        film_id,
+        film_title,
+        film_genre,
+        film_rating,
+        film_duration,
+        film_stereoscopes)
+        VALUES(
+        '2',
+        'Spaceballs',
+        'Sci-fi, Comedy',
+        'PG',
+        '1hr 36mins',
+        'standard'
+        );
+        INSERT INTO movies(
+        film_id,
+        film_title,
+        film_genre,
+        film_rating,
+        film_duration,
+        film_stereoscopes)
+        VALUES(
+        '3',
+        'The Fifth Element',
+        'Action,sci-fi,
+        'PG',
+        '1hr 25mins',
+        '3D'
+        );
+        INSERT INTO movies(
+        film_id,
+        film_title,
+        film_genre,
+        film_rating,
+        film_duration,
+        film_stereoscopes)
+        VALUES(
+        '4',
+        'The Nun 2',
+        'Horror',
+        'R',
+        '1hr 50mins',
+        'standard'
+        );
+        INSERT INTO movies(
+        film_id,
+        film_title,
+        film_genre,
+        film_rating,
+        film_duration,
+        film_stereoscopes)
+        VALUES(
+        '5',
+        'Barbie',
+        'Adventure, Comedy',
+        'PG-13',
+        '1hr 54mins',
+        'standard'
+        );
+        INSERT INTO movies(
+        film_id,
+        film_title,
+        film_genre,
+        film_rating,
+        film_duration,
+        film_stereoscopes)
+        VALUES(
+        '6',
+        'Talk To Me',
+        'Horror',
+        'R',
+        '1hr 35mins',
+        'standard'
+        );
+        INSERT INTO movies(
+        film_id,
+        film_title,
+        film_genre,
+        film_rating,
+        film_duration,
+        film_stereoscopes)
+        VALUES(
+        '7',
+        'TMNT: Mutant Mayhem',
+        'Children, Comedy, Adventure, Animated',
+        'PG',
+        '1hr 40mins',
+        'standard'
+        );
+        INSERT INTO movies(
+        film_id,
+        film_title,
+        film_genre,
+        film_rating,
+        film_duration,
+        film_stereoscopes)
+        VALUES(
+        '8',
+        'Blue Beetle',
+        'Action',
+        'PG-13',
+        '2hr 7mins',
+        'standard'
+        );
+INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '1',
+        'Dip-N-Dots',
+        '3.00',
+        '3.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '2',
+        'Popcorn',
+        '2.50',
+        '2.50'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '3',
+        'Pizza',
+        '4.00',
+        '4.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '4',
+        'Nachos and Cheese',
+        '4.00',
+        '4.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '5',
+        'Pretzels',
+        '1.50',
+        '1.50'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '6',
+        'slushies',
+        '2.00',
+        '2.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '7',
+        'candy',
+        '1.00',
+        '1.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '8',
+        'Chicken Tenders',
+        '5.00',
+        '5.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '9',
+        'Fries',
+        '3.00',
+        '3.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '10',
+        'hotdogs',
+        '1.50',
+        '1.50'
+        );
+INSERT INTO tickets(
+        ticket_id,
+        film_id,
+        ticket_cost,
+        total_amount,
+        customer_id)
+        Values(
+        '1',
+        '4',
+        '7.00',
+        '1'
+        );
+        INSERT INTO tickets(
+        ticket_id,
+        film_id,
+        ticket_cost,
+        total_amount,
+        customer_id)
+        Values(
+        '2',
+        '4',
+        '7.00',
+        '2'
+        );
---
+++ MT2.sql
@@ -0,0 +1,209 @@
+INSERT INTO movies(
+        film_id,
+        film_title,
+        film_genre,
+        film_rating,
+        film_duration,
+        film_stereoscopes)
+        VALUES(
+        '4',
+        'The Nun 2',
+        'Horror',
+        'R',
+        '1hr 50mins',
+        'standard'
+        );
+        INSERT INTO movies(
+        film_id,
+        film_title,
+        film_genre,
+        film_rating,
+        film_duration,
+        film_stereoscopes)
+        VALUES(
+        '5',
+        'Oppenheimer',
+        'Drama, Thriller',
+        'R',
+        '3 hours',
+        'standard'
+        );
+        INSERT INTO movies(
+        film_id,
+        film_title,
+        film_genre,
+        film_rating,
+        film_duration,
+        film_stereoscopes)
+        VALUES(
+        '6',
+        'Talk To Me',
+        'Horror',
+        'R',
+        '1hr 35mins',
+        'standard'
+        );
+        INSERT INTO movies(
+        film_id,
+        film_title,
+        film_genre,
+        film_rating,
+        film_duration,
+        film_stereoscopes)
+        VALUES(
+        '7',
+        'A Haunting in Venice',
+        'Mystery, Crime',
+        'PG-13',
+        '1hr 43mins',
+        'standard'
+        );
+        INSERT INTO movies(
+        film_id,
+        film_title,
+        film_genre,
+        film_rating,
+        film_duration,
+        film_stereoscopes)
+        VALUES(
+        '8',
+        'It Lives Inside',
+        'Horror',
+        'PG-13',
+        '1hr 39mins',
+        'standard'
+        );
+INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '1',
+        'Nachos with Cheese',
+        '3.00',
+        '3.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '2',
+        'Popcorn',
+        '2.50',
+        '2.50'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '3',
+        'Pizza',
+        '4.00',
+        '4.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '4',
+        'Corn dog',
+        '4.00',
+        '4.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '5',
+        'Curly Fries',
+        '1.50',
+        '1.50'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '6',
+        'Chips',
+        '2.00',
+        '2.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '7',
+        'candy',
+        '1.00',
+        '1.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '8',
+        'popcorn Chicken',
+        '5.00',
+        '5.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '9',
+        'Peanuts',
+        '3.00',
+        '3.00'
+        );
+        INSERT INTO consessions(
+        snack_id,
+        snack_name,
+        snack_amount,
+        total_amount)
+        VALUES(
+        '10',
+        'hotdogs',
+        '1.50',
+        '1.50'
+        );
+INSERT INTO tickets(
+        ticket_id,
+        film_id,
+        ticket_cost,
+        total_amount,
+        customer_id)
+        Values(
+        '1',
+        '4',
+        '7.00',
+        '1'
+        );
+        INSERT INTO tickets(
+        ticket_id,
+        film_id,
+        ticket_cost,
+        total_amount,
+        customer_id)
+        Values(
+        '2',
+        '4',
+        '7.00',
+        '2'
+        );
